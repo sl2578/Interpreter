e6a2dbca3c37c1431a2605987188c5fcedcf14c4
DanielWu <dgw47@cornell.edu>
2014-10-15 09:12:52 -0400

    fixed tabulate to return an array, implemented fold_left_imp, started writing test cases

--- mutability.ml
+++ mutability.ml
@@ -1,7 +1,9 @@
+open Array
 
 (* requires: two ints n and k
 returns: function type unit -> int
-calling the function returns value of n incremented by k once*)
+calling the function returns value of n incremented by k once
+effects: increases ref value of function by k *)
 let count_up_from n k =
 	let x = ref (n-k)
 	in (fun () -> x:=(!x + k); !x)
@@ -9,16 +11,24 @@ let count_up_from n k =
 (* requires: f a pure function and an int n
 returns: array of length n, each index i has the value f i *)
 let tabulate f n = 
+	(* build up a list of length n *)
 	let rec make_index_list (i, acc) =
 		if i < 0 then acc else make_index_list (i-1, (f i)::acc) 
-	in make_index_list ((n-1), [])
+	(* convert list to array *)
+	in Array.of_list (make_index_list ((n-1), []))
 
-
-(* requires: f a pure function, an initial accumulator,
-	and list of elements to fold over
-returns: accumulated result of folding over a list,
-applying f to each element *)
-let fold_left_imp f acc xs = failwith "TODO"
+(* Implementation of fold_left without recursion
+requires: f a pure function, initial 'a accumulator, and 'b list to fold over
+returns: value of 'a accumulator after applying f to acc and each element *)
+let fold_left_imp f acc xs =
+	(* turn acc into reference *)
+	let acc = ref acc in
+	(* turn xs into array *)
+	let xs = Array.of_list xs in
+	(* loop through the list *)
+	for i = 0 to ((Array.length xs) - 1) do
+		acc := f !acc xs.(i)
+	done; !acc (* return final value of acc *)
 
 type t = unit  (* TODO: change unit to whatever you want *)
 type u = unit  (* TODO: change unit to whatever you want *)
