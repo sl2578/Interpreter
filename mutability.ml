8452ec7460644b6484ed3c575d45e17aa4e7fbc6
DanielWu <dgw47@cornell.edu>
2014-10-15 07:57:46 -0400

    wrote comments for some functions

--- mutability.ml
+++ mutability.ml
@@ -1,12 +1,23 @@
+
+(* requires: two ints n and k
+returns: function type unit -> int
+calling the function returns value of n incremented by k once*)
 let count_up_from n k =
 	let x = ref (n-k)
 	in (fun () -> x:=(!x + k); !x)
 
+(* requires: f a pure function and an int n
+returns: array of length n, each index i has the value f i *)
 let tabulate f n = 
 	let rec make_index_list (i, acc) =
 		if i < 0 then acc else make_index_list (i-1, (f i)::acc) 
 	in make_index_list ((n-1), [])
 
+
+(* requires: f a pure function, an initial accumulator,
+	and list of elements to fold over
+returns: accumulated result of folding over a list,
+applying f to each element *)
 let fold_left_imp f acc xs = failwith "TODO"
 
 type t = unit  (* TODO: change unit to whatever you want *)
