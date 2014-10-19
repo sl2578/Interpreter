57e5244e0690fd5d3aec609ea0316d1d4bba90d5
DanielWu <dgw47@cornell.edu>
2014-10-18 21:35:47 -0400

    wrote test for zardoz, moved mli file for iterator

--- mutability_test.ml
+++ mutability_test.ml
@@ -2,3 +2,4 @@ open Assertions
 open Mutability
 
 TEST_UNIT "fold_left_imp_test1" = assert_true ((fold_left_imp (fun acc elm -> acc + elm) 0 [1;2;3]) = 6)
+TEST_UNIT "zardoz_test" = assert_false (List.map zardoz (List.rev lst) = List.rev (List.map zardoz lst))
