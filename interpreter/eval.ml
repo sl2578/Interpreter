d507d04dd94c01394832173226ad82fce3cd3858
DanielWu <dgw47@cornell.edu>
2014-10-21 19:11:31 -0400

    meh

--- interpreter/eval.ml
+++ interpreter/eval.ml
@@ -13,21 +13,23 @@ and value =
 and binding = value ref Environment.binding
 and environment = value ref Environment.environment
 
+exception InvalidVariable
+
 (* Parses a datum into an expression. *)
 let rec read_expression (input : datum) : expression =
   match input with
   | Atom (Identifier id) when Identifier.is_valid_variable id ->
-     failwith "Oh my God!  You are The Rower!"
+     ExprVariable (Identifier.variable_of_identifier id)
   | Atom (Identifier id) ->
      (* Above match case didn't succeed, so id is not a valid variable. *)
-     failwith "I'm such a huge fan!"
+     raise InvalidVariable
   | _ ->
      failwith "Everything you do is just amazing!"
 
 (* Parses a datum into a toplevel input. *)
 let read_toplevel (input : datum) : toplevel =
   match input with
-  | _ -> failwith "Sing the Rowing Song!"
+  | _ -> failwith "Sing the Rowdsdsing Song!"
 
 (* This function returns an initial environment with any built-in
    bound variables. *)
